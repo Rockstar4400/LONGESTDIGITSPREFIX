d8b8c11faf584e22f4ebab2924bd23eb5c69326a
Alberto Del Angel <rockstar4400@gmail.com>
2020-08-29 20:27:41 -0500

    Codesignal

--- longestDigitsPrefix.java
+++ longestDigitsPrefix.java
@@ -1,6 +1,8 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+//Source help: http://tutorials.jenkov.com/java-regex/matcher.html
+
 public class longestDigitsPrefix {
 
     static String longestDigitsPrefix(String inputString) {
