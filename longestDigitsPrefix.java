ca6d76189a6d1c6c3ad16a58652dcf6fa2238760
Alberto Del Angel <rockstar4400@gmail.com>
2020-08-29 20:16:54 -0500

    Codesignal

---
+++ longestDigitsPrefix.java
@@ -0,0 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class longestDigitsPrefix {
+
+    static String longestDigitsPrefix(String inputString) {
+        Pattern p = Pattern.compile("^\\d*\\d");
+        Matcher m = p.matcher(inputString);
+        String result = "";
+        int count = 0;
+        while(m.find()) {
+            count++;
+            result = inputString.substring(m.start(),m.end());
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(longestDigitsPrefix("123aaa1"));// "123"
+    }
+}
